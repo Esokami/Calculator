4d857874ee18e5e5a2c120abf9f9847c119bc406
Esokami <mollica.es@gmail.com>
2021-12-31 10:58:15 -0800

    Updated CSS

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 var displayDiv = document.querySelector("#display");
 displayDiv.innerText = "Some new value";
+var result = 0; 
+
+function setOP(){
+
+}
+
+function clr(){
+    result = 0;
+    displayDiv.innerText = result;
+}
